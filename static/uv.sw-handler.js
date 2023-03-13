9c00a485a9552a2d38cf83dfefa124a853777925
0xkkl <kaydan@leozc.com>
2023-03-12 18:11:13 -0700

    added elixir and utopia

--- static/uv.sw-handler.js
+++ static/uv.sw-handler.js
@@ -1,9 +1,11 @@
 importScripts('/uv/uv.bundle.js');
 importScripts('/uv/uv.config.js');
-importScripts(__uv$config.sw || '/uv/uv.sw.js');
-
+importScripts("/uv/uv.sw.js");
+importScripts("./nebula/osana/osana.worker.js");
 const sw = new UVServiceWorker();
 
-self.addEventListener('fetch', event => {
-    event.respondWith(sw.fetch(event))
+
+self.addEventListener("fetch", (event) => {
+    if (event.request.url.startsWith(location.origin + "/service/"))
+      event.respondWith(UV.fetch(event));
 });
