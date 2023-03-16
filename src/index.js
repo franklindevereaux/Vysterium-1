79602b63f2c7f211e5e200fe7fabe22dd3fb4153
urasdbwd <102772348+urasdbwd@users.noreply.github.com>
2023-03-15 20:16:32 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,10 +5,6 @@ import { createServer as createHttpServer } from "node:http";
 import { readFileSync, existsSync } from "node:fs";
 import { hostname } from "node:os";
 import serveStatic from "serve-static";
-
-// The following message MAY NOT be removed
-console.log("Incognito\nThis program comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it\nunder the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\n\nYou should have received a copy of the GNU General Public License\nalong with this program. If not, see <https://www.gnu.org/licenses/>.\n");
-
 const bare = createBareServer("/bare/");
 const serve = serveStatic(fileURLToPath(new URL("../static/", import.meta.url)), { fallthrough: false });
 var server, PORT = process.env.PORT;
@@ -18,7 +14,7 @@ if(ssl) {
     key: readFileSync("../ssl/key.pem"),
     cert: readFileSync("../ssl/cert.pem")
   });
-  PORT = (PORT || 443);
+  PORT = (PORT || 80);
 } else { server = createHttpServer(); PORT = (PORT || 8080);}
 
 server.on("request", (req, res) => {
